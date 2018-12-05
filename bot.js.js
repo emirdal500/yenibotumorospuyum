0794ec5fbc69449ee60b1bef84518d38163e78b3
emirdal1020 <45396268+emirdal1020@users.noreply.github.com>
2018-12-05 04:44:45 +0300

    Update bot.js.js

--- bot.js.js
+++ bot.js.js
@@ -96,9 +96,9 @@ client.on("guildMemberAdd", member => {
 	
 	member.addRole(role); 
 	
-	channel.send(member + " artık " + role + " rolü ile aramızda https://cdn.discordapp.com/attachments/516675904374702091/519687343787999232/hosgeldiniz-resmi.gif");
+	channel.send(member + " artık " + role + " rolü ile aramızda");
 	
-	member.send("Aramıza hoş geldin! Seni Gördüğümüze Çok Sevindik. https://cdn.discordapp.com/attachments/516675904374702091/519687343787999232/hosgeldiniz-resmi.gif")
+	member.send("Aramıza hoş geldin! Seni Gördüğümüze Çok Sevindik.")
 	
 });
 
