e3c4a0cc4a19087e232cc507bfefe03d55b26bb0
emirdal1020 <45396268+emirdal1020@users.noreply.github.com>
2018-12-06 20:50:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -162,16 +162,4 @@ client.on('error', e => {
   console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
 
-if (msg.content === 'espri') {
- if (Math.floor((Math.random () * 3) + 1) === 1) {
-   msg.channel.send('Yağmur Yağmış, Kar Bal.');
-}else if (Math.floor((Math.random () * 3) + 1) === 2) {
-   msg.channel.send('Elektrigi Edison buldu ama parasini niye biz ödüyoruz.');
-}else if (Math.floor((Math.random () * 3) + 1) === 3) {
-   msg.channel.send('Yoldan fıstık Gibi Kız  Geçiyormuş Kıza Araba çarpmış Nolmuş?\n-Fıstık Ezmesi');
-  }
-}
-
-});
-
 client.login(process.env.BOT_TOKEN);
