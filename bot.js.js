e031e284968130e7d8e2f714271773bfb0457832
emirdal1020 <45396268+emirdal1020@users.noreply.github.com>
2018-12-06 20:30:31 +0300

    Update bot.js.js

--- bot.js.js
+++ bot.js.js
@@ -164,11 +164,11 @@ client.on('error', e => {
 
 if {msg.content === '-espri') {
  if (Math.floor((Math.random () * 3) + 1) === 1) {
-  msg.channel.send('Yağmur Yağmış, Kar Bal.');
+   msg.channel.send('Yağmur Yağmış, Kar Bal.');
 }else if (Math.floor((Math.random () * 3) + 1) === 2) {
-  msg.channel.send('Elektrigi Edison buldu ama parasini niye biz ödüyoruz.');
+   msg.channel.send('Elektrigi Edison buldu ama parasini niye biz ödüyoruz.');
 }else if (Math.floor((Math.random () * 3) + 1) === 3) {
-  msg.channel.send('Yoldan fıstık Gibi Kız  Geçiyormuş Kıza Araba çarpmış Nolmuş?\n-Fıstık Ezmesi');
+   msg.channel.send('Yoldan fıstık Gibi Kız  Geçiyormuş Kıza Araba çarpmış Nolmuş?\n-Fıstık Ezmesi');
   }
 }
 
