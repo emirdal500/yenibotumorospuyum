afc0b15c58227107e6841d69fbee093041a7d24b
emirdal1020 <45396268+emirdal1020@users.noreply.github.com>
2018-12-13 22:21:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -136,28 +136,6 @@ message.channel.send(level)
   }
 });
 
-  db.fetch(`kufur_${msg.guild.id}`).then(i => {
-    if (i == 'acik') {
-        const kufur = ["oç", "amk", "ananı sikiyim", "ananıskm", "piç", "amk", "amsk", "sikim", "sikiyim", "orospu çocuğu", "piç kurusu", "kahpe", "orospu", "mal", "sik", "yarrak", "am", "amcık", "amık", "yarram", "sikimi ye", "mk", "mq", "aq", "ak", "amq",];
-        if (kufur.some(word => msg.content.includes(word))) {
-          try {
-            if (!msg.member.hasPermission("BAN_MEMBERS")) {
-                  msg.delete();
-
-                  return msg.reply('Küfür etmemelisin! ⚠').then(msg => msg.delete(3000));
-            }              
-          } catch(err) {
-            console.log(err);
-          }
-        }
-    }
-    else if (i == 'kapali') {
-      
-    }
-    if (!i) return;
-  })
-    });
-
 exports.run = (client, message, args) => {
     message.channel.send("Psst. YakÄ±yorum.").then(async msg => {
                     setTimeout(() => {
