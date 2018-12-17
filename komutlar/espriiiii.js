c7034e6c0c7b77b2bc421fd2aa30e73f1aadb779
emirdal1020 <45396268+emirdal1020@users.noreply.github.com>
2018-12-18 00:37:39 +0300

    Update espriiiii.js

--- komutlar/espriiiii.js
+++ komutlar/espriiiii.js
@@ -2,7 +2,7 @@
 
  exports.run = (client, message, args) => {
    var cevaplar = ['Oda Beni Seviyor, Ama koridordan emin değilim. :smile:','Hatayı Kendinizde Aramayın, Hatay Akdeniz Bölgesindedir. :smile:','1. Sınav Kolaydı Ama 2. Sınav Meyvesuyu :smile:','Adamın Evi Yanmış Odaları Düz. :smile:','Seni Aramazsam... Funda Arar :smile:','Hakan Peker, Tahin Pekmez :smile:','Adamın Gözü Dalmış Kulakları Yaprak. :smile:','İngilizler, Ben İzlemem Moruk',
-   'Seven Gitmez Oğlum, Eight Gider.','Yoldan Fıstık Gibi Kız Geçiyormuş Kıza Araba Çarpmış Nolmuş? Fıstık Ezmesi.','Tuzu Uzatırmısın Kanka, Tuuuuuuuuuuuz','Rıdvanın bir büyüğü kimdir? Rıdtwo. :smile:','Tebrikler Kazandınız! şimdide tencere oldunuz.','Adamın Bacakları Atmış Kolları Eşşek. :smile:','Kelebekler Ben Beklemem ];
+   'Seven Gitmez Oğlum, Eight Gider.','Yoldan Fıstık Gibi Kız Geçiyormuş Kıza Araba Çarpmış Nolmuş? Fıstık Ezmesi.','Tuzu Uzatırmısın Kanka, Tuuuuuuuuuuuz','İspanyollar Ben Yollamam Kanka','Uzun Lafın Kısası : U.L','Bu erikson, başka erik yok','Rıdvanın bir büyüğü kimdir? Rıdtwo'];
    var cevap = cevaplar[Math.floor(Math.random() * cevaplar.length)];
    message.reply(cevap);
 };
