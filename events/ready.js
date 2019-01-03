0cf29a74e2b723537473404fa233dec9a3a4dbc1
emirdal1020 <45396268+emirdal1020@users.noreply.github.com>
2019-01-04 02:46:45 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -13,3 +13,53 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 };
+
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+console.log('>>Oynuyor kısmı başarıyla güncellendi.');
+console.log('Bot hazır ve giriş yaptı Üstadım.');
+
+    var Games = [
+
+        "🔥Yeni Yardım Menüm yayında e/yardım🔥",
+    
+        "🔥e/davet Botu ekleyin🔥",
+        
+        "🔥e/slowmode Aktif🔥",
+        
+        "🔥e/mesajküçült Aktif🔥",
+        
+        "🔥e/fakemesaj Aktif🔥",
+        
+        "🔥e/ Aktif🔥",
+        
+        "🔥e/kralol Aktif 🔥",
+        "🔥NSFW komutları Aktif🔥",
+        
+        `${client.users.size} Kullanıcı İçin Teşekkürler`,
+        
+        `${client.guilds.size} Sunucu İçin Teşekkürler`,
+        
+       
+
+
+
+        `🔥Yapımcım Izmirli Cocuk#0001`
+
+
+    ];
+
+    setInterval(function() {
+
+        var random = Math.floor(Math.random()*(Games.length-0+1)+0);
+
+        client.user.setGame(Games[random], "https://www.twitch.tv/EmirDal40");
+        }, 2 * 2500);
+
+};  
